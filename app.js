42f4b11929dd689e9fe218d0c1b34da4be9df888
John deGuise <jrd120893@gmail.com>
2018-06-16 16:08:44 -0600

    Updates app.js with postgres db support and to text parameters to db

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const twilioClient = require('twilio')(config.ACCOUNT_SID, config.AUTH_TOKEN);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
 const twiml = new MessagingResponse();
+
+const { Client } = require('pg');
+const postgresClient = new Client({
+  host: config.POSTGRES_DB_HOST,
+  port: config.POSTGRES_DB_PORT,
+  user: config.POSTGRES_DB_USER,
+  password: config.POSTGRES_DB_PASS,
+  database: config.POSTGRES_DB_NAME
+});
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 function sendText(message) {
   twiml.message(message);
@@ -27,17 +37,49 @@ function buildTextMessage(
   return message;
 }
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/sms", function(req, res) {
+
   // Response must be formatted as: 1-4; 396; 3.2mi; 31:30
   const responseDetails = req.body.Body.split('; ');
   const runningCycleOnDays = 3;
+
+  postgresClient.connect((err) => {
+    if (err) {
+      console.log('connection error: ' + err.stack);
+    }
+    else {
+      const shouldAbort = (err) => {
+        if (err) {
+          console.error('Error in transaction', err.stack);
+          postgresClient.query('ROLLBACK', (err) => {
+            if (err) {
+              console.error('Error rolling back client', err.stack);
+            }
+          });
+        }
+        return !!err
+      }
+      postgresClient.query('BEGIN', (err) => {
+        if (shouldAbort(err)) return;
+
+        postgresClient.query("INSERT into dailyfitnessstatistics(dayincycle, caloriesburned, distanceran, runningtime) VALUES($1, $2, $3, $4) RETURNING id",
+          [responseDetails[0], responseDetails[1], responseDetails[2], responseDetails[3]], (err, res) => {
+            if (shouldAbort(err)) return;
+
+            postgresClient.query('COMMIT', (err) => {
+              if (err) {
+                console.error('Error committing transaction', err.stack);
+              }
+            })
+        });
+      });
+    }
+  });
 
   sendText(
     buildTextMessage(responseDetails[0],responseDetails[1],
       responseDetails[2], responseDetails[3], runningCycleOnDays)
   );
-
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
