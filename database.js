f34d99c426ef3b0b2c0d30d2687e4446e8df83f4
John deGuise <jrd120893@gmail.com>
2018-06-23 14:28:51 -0600

    Whitespace issues

--- database.js
+++ database.js
@@ -4,11 +4,11 @@ const configJS = require('./config');
 const config = {
 	max: 10,
 	idleTimeoutMillis: 30000,
-    host: configJS.POSTGRES_DB_HOST,
-    port: configJS.POSTGRES_DB_PORT,
-    user: configJS.POSTGRES_DB_USER,
-    password: configJS.POSTGRES_DB_PASS,
-    database: configJS.POSTGRES_DB_NAME
+	host: configJS.POSTGRES_DB_HOST,
+	port: configJS.POSTGRES_DB_PORT,
+	user: configJS.POSTGRES_DB_USER,
+	password: configJS.POSTGRES_DB_PASS,
+	database: configJS.POSTGRES_DB_NAME
 };
 
 module.exports = new pg.Pool(config);
