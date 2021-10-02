94f52b7a25379a7ccc4c1f5d62a883dd48fbe8ec
John deGuise <john.r.deguise@gmail.com>
2021-10-02 17:36:40 -0600

    updates to config.js

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  SERVER_PORT: 'Your-HTTP-Server-Port',
-  ACCOUNT_SID: 'Your-Twilio-Account-SID',
-  AUTH_TOKEN: 'Your-Twilio-Auth-Token',
   POSTGRES_DB_HOST: 'Your-Postgres-DB-Host',
   POSTGRES_DB_PORT: 'Your-Postgres-DB-Port',
   POSTGRES_DB_USER: 'Your-Postgres-DB-User',
   POSTGRES_DB_PASS: 'Your-Postgres-DB-Pass',
-  POSTGRES_DB_NAME: 'Your-Postgres-DB-Name'
+  POSTGRES_DB_NAME: 'Your-Postgres-DB-Name',
+  PHONE_NUMBER_EMAIL: '5555551234@carrierdomain.tld'
 };
